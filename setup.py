7d072bede174c383ffdddd4a2b91fe472ad4155b
Sergiu <sergio.ghior@gmail.com>
2023-02-24 22:28:29 +0100

    fix: added lightweight-charts.standalone.production.js file to setup

--- setup.py
+++ setup.py
@@ -150,6 +150,7 @@ setup(
     # http://docs.python.org/distutils/setupscript.html#installing-additional-files
     #
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    data_files=[("lightweight-charts.standalone.production.js", "src/lightweight-charts.standalone.production.js")]
     # data_files=[("my_data", ["data/data_file"])],  # Optional
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
